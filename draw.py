d13702d045adac6ce7fb330c18bd5ee90acd7abd
overxus <overxus@invalid.com>
2024-02-20 19:58:05 +0800

    三角形颜色插值

--- draw.py
+++ draw.py
@@ -1,4 +1,8 @@
+from display import Display
+
+
 Point = tuple[int, int]
+Color = tuple[int, int, int, int]
 
 
 def interval(x1: int, x2: int):
@@ -25,24 +29,32 @@ def interpolate(x1: int, y1: float, x2: int, y2: float):
     return result
 
 
-def drawLine(p1: Point, p2: Point) -> list[Point]:
+def drawLine(display: Display, p1, color1, p2, color2):
     x1, y1 = p1
     x2, y2 = p2
-
-    if abs(y1 - y2) > abs(x1 - x2):
-        return [(int(x), y) for x, y in zip(interpolate(y1, x1, y2, x2), interval(y1, y2))]
+    if x1 == x2:
+        for y, alpha in zip(interval(y1, y2), interpolate(y1, 1.0, y2, 0.0)):
+            display.drawPixel(x1, y, alpha * color1 + (1 - alpha) * color2)
+    elif y1 == y2:
+        for x, alpha in zip(interval(x1, x2), interpolate(x1, 1.0, x2, 0.0)):
+            display.drawPixel(x, y1, alpha * color1 + (1 - alpha) * color2)
+    elif abs(y1 - y2) > abs(x1 - x2):
+        for x, y, alpha in zip(interpolate(y1, x1, y2, x2), interval(y1, y2), interpolate(y1, 1.0, y2, 0.0)):
+            color = alpha * color1 + (1 - alpha) * color2
+            display.drawPixel(int(x), y, color)
     else:
-        return [(x, int(y)) for x, y in zip(interval(x1, x2), interpolate(x1, y1, x2, y2))]
+        for x, y, alpha in zip(interval(x1, x2), interpolate(x1, y1, x2, y2), interpolate(y1, 1.0, y2, 0.0)):
+            color = alpha * color1 + (1 - alpha) * color2
+            display.drawPixel(x, int(y), color)
 
 
-def drawTriangle(p1: Point, p2: Point, p3: Point) -> list[Point]:
-    result = drawLine(p1, p2)
-    result += drawLine(p2, p3)
-    result += drawLine(p1, p3)
-    return result
+def drawTriangle(display: Display, p1, color1, p2, color2, p3, color3):
+    drawLine(display, p1, color1, p2, color2)
+    drawLine(display, p2, color2, p3, color3)
+    drawLine(display, p1, color1, p3, color3)
 
 
-def drawFilledTriangle(p1: Point, p2: Point, p3: Point) -> list[Point]:
+def drawFilledTriangle(display: Display, p1, color1, p2, color2, p3, color3):
     """绘制填充的三角形"""
     x1, y1 = p1
     x2, y2 = p2
@@ -59,13 +71,17 @@ def drawFilledTriangle(p1: Point, p2: Point, p3: Point) -> list[Point]:
         y2, y3 = y3, y2
     
     x13 = interpolate(y1, x1, y3, x3)
+    c13 = [alpha * color1 + (1 - alpha) * color3 for alpha in interpolate(y1, 1.0, y3, 0.0)]
     x12 = interpolate(y1, x1, y2, x2)
+    c12 = [alpha * color1 + (1 - alpha) * color2 for alpha in interpolate(y1, 1.0, y2, 0.0)]
     x23 = interpolate(y2, x2, y3, x3)
+    c23 = [alpha * color2 + (1 - alpha) * color3 for alpha in interpolate(y2, 1.0, y3, 0.0)]
     x12.pop()
+    c12.pop()
     x123 = x12 + x23
+    c123 = c12 + c23
 
-    result = []
     for idx, y in enumerate(interval(y1, y3)):
-        for x in interval(int(x123[idx]), int(x13[idx])):
-            result.append((x, y))
-    return result
+        px1, px2 = int(x123[idx]), int(x13[idx])
+        cx1, cx2 = c123[idx], c13[idx]
+        drawLine(display, (px1, y), cx1, (px2, y), cx2) 
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import os
+import numpy as np
 
 from display import Display
 import draw
+
+
+def make_color(r: int, g: int, b: int, a: int = 255):
+    return np.array((r, g, b, a), dtype=np.uint8)
 
 
 width = 200
@@ -9,11 +14,13 @@ height = 100
 
 display = Display(width, height)
 p1 = (width // 3, height // 3)
+c1 = make_color(255, 0, 0, 255)
 p2 = (width * 2 // 3, height // 3)
+c2 = make_color(0, 255, 0, 255)
 p3 = (width // 2, height * 2 // 3)
+c3 = make_color(0, 0, 255, 255)
 
-for x, y in draw.drawFilledTriangle(p1, p2, p3):
-    display.drawPixel(x, y, (255, 0, 0, 255))
+draw.drawFilledTriangle(display, p1, c1, p2, c2, p3, c3)
 
 os.makedirs('output', exist_ok=True)
 display.save('output/drawTriangle.png')
